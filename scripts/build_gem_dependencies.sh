2b4e49f7c339a80ad26cc8c203f0279c985f71f6
Andrew Lohr <ajlohr@us.ibm.com>
2019-12-16 12:25:24 -0500

    update gemfile lock with bundler 2.1.0

--- scripts/build_gem_dependencies.sh
+++ scripts/build_gem_dependencies.sh
@@ -1,5 +1,4 @@
-echo "Installing ruby packages..."
-# only install the latest 1.x version of bundler. We had problems with 2.x
-# the problems might be fixes in later bundler versions so we can try again later
-gem install bundler -v "~>1.0"
+echo "Installing bundler and ruby packages..."
+# Install the lastest 2.x version of bundler
+gem install bundler -v "~>2.0"
 bundle install
