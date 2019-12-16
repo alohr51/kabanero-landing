fb5cc19c941e31fc98fde43deb14e0abe992e266
Andrew Lohr <ajlohr@us.ibm.com>
2019-12-16 12:34:24 -0500

    use bundler 2.0.2 explicitly

--- scripts/build_gem_dependencies.sh
+++ scripts/build_gem_dependencies.sh
@@ -1,4 +1,4 @@
 echo "Installing bundler and ruby packages..."
-# Install the lastest 2.x version of bundler
-gem install bundler -v "~>2.0"
+# Install bundler 2.0.2 because 2.1.x is breaking the build
+gem install bundler -v "2.0.2"
 bundle install
